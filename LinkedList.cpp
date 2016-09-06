5b47223248ae892cafa8d53b84a22b7b45d41365
Terrance Griffith <tjgriffi@ucsd.edu>
2016-09-05 20:37:56 -0700

    Added kthToLast method

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,6 +5,32 @@ public class LinkedList{
 		LinkedListNode _next;
 		int _data;
 
-		public LinkedListNode(int data)
+		public LinkedListNode(int data){
+			_data = data;
+		}
+	}
+	
+	LinkedListNode head;
+	
+	//Finds the kth to last node in a linked list
+	LinkedListNode kthToLast( LinkedListNode n, int k){
+		LinkedListNode curr = n;
+		LinkedListNode kBefore = n;
+		
+		//Get curr k nodes from the first node
+		for (int i = 0; i < k; i++){
+			if(curr.next == null){
+				return ERROR;
+			}
+			
+			curr = curr.next;
+		}
+		
+		while(curr != null){
+			curr = curr.next;
+			kBefore = kBefore.next;
+		}
+		
+		return kBefore;
 	}
 }
